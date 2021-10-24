f73053d75becb109aa70ebf8a851ac818bdd80b0
Robert <soreterobert@gmail.com>
2021-10-24 21:50:43 +0300

    CMakeLists

---
+++ cmake-build-debug/CMakeFiles/Modern_Chess.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Modern_Chess.dir/main.cpp.obj"
+  "Modern_Chess.exe"
+  "Modern_Chess.exe.manifest"
+  "Modern_Chess.pdb"
+  "libModern_Chess.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Modern_Chess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
