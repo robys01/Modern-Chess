c850173a46e8b64439efab6f16ce80e0d31a3348
Robert <soreterobert@gmail.com>
2021-12-13 02:19:23 +0200

    Textures Class inbound

---
+++ headers/Textures.h
@@ -0,0 +1,31 @@
+//
+// Created by Robert on 12/13/2021.
+//
+
+#ifndef MODERN_CHESS_TEXTURES_H
+#define MODERN_CHESS_TEXTURES_H
+
+
+#include <SFML/Graphics.hpp>
+
+class Textures {
+public:
+    static sf::Texture whitePawn;
+    static sf::Texture whiteKnight;
+    static sf::Texture whiteBishop;
+    static sf::Texture whiteRook;
+    static sf::Texture whiteQueen;
+    static sf::Texture whiteKing;
+
+    static sf::Texture blackPawn;
+    static sf::Texture blackKnight;
+    static sf::Texture blackBishop;
+    static sf::Texture blackRook;
+    static sf::Texture blackQueen;
+    static sf::Texture blackKing;
+
+    static sf::Texture loadTexture(std::string str);
+};
+
+
+#endif //MODERN_CHESS_TEXTURES_H
---
+++ src/Textures.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Robert on 12/13/2021.
+//
+
+#include <iostream>
+#include "../headers/Textures.h"
+
+sf::Texture Textures::loadTexture(std::string str) {
+    sf::Texture texture;
+    if (!texture.loadFromFile(str))
+        std::cout << "Failed to open" << str << '\n';
+    return texture;
+}
+
+sf::Texture Textures::whitePawn = Textures::loadTexture("resources/wP.png");
+sf::Texture Textures::whiteKnight = Textures::loadTexture("resources/wN.png");
+sf::Texture Textures::whiteBishop = Textures::loadTexture("resources/wB.png");
+sf::Texture Textures::whiteRook = Textures::loadTexture("resources/wR.png");
+sf::Texture Textures::whiteQueen = Textures::loadTexture("resources/wQ.png");
+sf::Texture Textures::whiteKing = Textures::loadTexture("resources/wK.png");
+
+sf::Texture Textures::blackPawn = Textures::loadTexture("resources/bP.png");
+sf::Texture Textures::blackKnight = Textures::loadTexture("resources/bN.png");
+sf::Texture Textures::blackBishop = Textures::loadTexture("resources/bB.png");
+sf::Texture Textures::blackRook = Textures::loadTexture("resources/bR.png");
+sf::Texture Textures::blackQueen = Textures::loadTexture("resources/bQ.png");
+sf::Texture Textures::blackKing = Textures::loadTexture("resources/bK.png");
