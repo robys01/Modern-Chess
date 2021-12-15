4a0f5a5c6f3d878e13b12c0d31b3256a4301263f
Robert <soreterobert@gmail.com>
2021-12-15 22:38:58 +0200

    Class Piece and Inheritances

--- headers/Piece.h
+++ headers/Piece.h
@@ -7,42 +7,80 @@
 
 #include "Textures.h"
 
-enum class PieceType {
-    Empty, Pawn, Knight, Bishop, Rook, Queen, King
-};
+
 enum class Side {
-    EMPTY, BLACK, WHITE
+    EMPTY = 0, BLACK = 1, WHITE = 2
 };
 
-
-
+/// Can make class Piece absctract class, but I will use class Piece as an empty space
 class Piece {
 protected:
     int code;
-    PieceType pieceType;
     Side side;
 
-    sf::Texture pTexture;
     sf::Sprite pSprite;
-
-//    float squareSize = 64;
-    float pieceSize = 60;
 
 public:
     Piece();
-    Piece(PieceType pieceType, Side side);
-    Piece(const Piece &piece);
-    ~Piece();
+    virtual ~Piece();
+    virtual void canMove();
 
-    int loadTextures();
     void drawPiece(sf::RenderWindow &window);
     void setPosition(float x, float y);
     void setScale(float x, float y);
-    int getCode() const;
+    void setOrigin(float x, float y);
 
-    Piece operator=(const Piece &piece);
-    friend std::ostream &operator<<(std::ostream &os, const Piece &piece);
+    friend std::ostream& operator<<(std::ostream& os, const Piece& piece);
+};
 
+
+class Pawn : public Piece {
+
+public:
+    Pawn(const Side& side);
+    ~Pawn() override;
+    void canMove() override;
+
+};
+
+
+class Knight : public Piece {
+public:
+    Knight(const Side& side);
+    ~Knight() override;
+    void canMove() override;
+};
+
+
+class Bishop : public Piece {
+public:
+    Bishop( const Side& side);
+    ~Bishop() override;
+    void canMove() override;
+};
+
+
+class Rook : public Piece {
+public:
+    Rook(const Side& side);
+    ~Rook() override;
+    void canMove() override;
+};
+
+
+class Queen : public Piece {
+public:
+    Queen(const Side& side);
+    ~Queen() override;
+    void canMove() override;
+};
+
+
+class King : public Piece {
+public:
+    King(const Side& side);
+    ~King() override;
+    void canMove() override;
 };
 
 
--- src/Piece.cpp
+++ src/Piece.cpp
@@ -5,91 +5,133 @@
 #include <iostream>
 
 
-Piece::Piece(){
+/// Class Piece
+Piece::Piece() {
+    code = 0;
+    side = Side::EMPTY;
 
 }
-
-Piece::Piece(PieceType pieceType, Side side) {
-
-    switch(pieceType) {
-        case PieceType::Pawn:
-            code = (1 << 2) + 1 + (side == Side::WHITE);
-            pTexture = side == Side::WHITE ? Textures::whitePawn : Textures::blackPawn;
-            break;
-        case PieceType::Knight:
-            code = (1 << 3) + 1 + (side == Side::WHITE);
-            pTexture = side == Side::WHITE ? Textures::whiteKnight : Textures::blackKnight;
-            break;
-        case PieceType::Bishop:
-            code = (1 << 4) + 1 + (side == Side::WHITE);
-            pTexture = side == Side::WHITE ? Textures::whiteBishop : Textures::blackBishop;
-            break;
-        case PieceType::Rook:
-            code = (1 << 5) + 1 + (side == Side::WHITE);
-            pTexture = side == Side::WHITE ? Textures::whiteRook : Textures::blackRook;
-            break;
-        case PieceType::Queen:
-            code = (1 << 6) + 1 + (side == Side::WHITE);
-            pTexture = side == Side::WHITE ? Textures::whiteQueen : Textures::blackQueen;
-            break;
-        case PieceType::King:
-            code = (1 << 7) + 1 + (side == Side::WHITE);
-            pTexture = side == Side::WHITE ? Textures::whiteKing : Textures::blackKing;
-            break;
-        case PieceType::Empty:
-            code = 0;
-            break;
-    }
-//    pieceSize = (float)(pTexture.getSize().x + pTexture.getSize().y) / 2.0f;
-
-    pSprite.setOrigin(pieceSize / 2.0f, pieceSize / 2.0f);
-//    pSprite.setColor((side == Side::WHITE ? sf::Color::Green : sf::Color::Red));
-    pTexture.setSmooth(true);
-    pSprite.setTexture(pTexture);
-
-}
-Piece::Piece(const Piece &piece) {
-    code = piece.code;
-    pieceType = piece.pieceType;
-    side = piece.side;
-
-    pTexture = piece.pTexture;
-    pSprite = piece.pSprite;        // Both of them are required, Sprite operator= doesn't
-    pSprite.setTexture(pTexture);   // copy the image, resulting a white square
-}
-
 
 Piece::~Piece() = default;
 
-void Piece::drawPiece(sf::RenderWindow &window) {
-    window.draw(pSprite);
+void Piece::canMove() {
+    std::cout << "Ce sa muti?\n";
 }
 
 void Piece::setPosition(float x, float y) {
     pSprite.setPosition(x, y);
 }
 
-void Piece::setScale(float x, float y) {
-    pSprite.setScale(x, y);
-}
-
-int Piece::getCode() const {
-    return code;
-}
-
-Piece Piece::operator=(const Piece &piece) {
-    code = piece.code;
-    pieceType = piece.pieceType;
-    side = piece.side;
-
-    pTexture = piece.pTexture;
-    pSprite = piece.pSprite;        // Both of them are required, Sprite's operator= doesn't
-    pSprite.setTexture(pTexture);   // copy the image, resulting a white square
-
-    return *this;
+void Piece::drawPiece(sf::RenderWindow &window) {
+    window.draw(pSprite);
 }
 
 std::ostream &operator<<(std::ostream &os, const Piece &piece) {
     os << piece.code;
     return os;
+}
+
+void Piece::setScale(float x, float y) {
+    pSprite.setScale(x, y);
+}
+
+void Piece::setOrigin(float x, float y) {
+    pSprite.setOrigin(x, y);
+}
+
+
+/// Class Pawn
+Pawn::Pawn(const Side &side) {
+    this->side = side;
+    code = (1 << 2) + (int) side;
+    pSprite.setTexture(side == Side::WHITE ? Textures::whitePawn : Textures::blackPawn);
+    pSprite.setOrigin(pSprite.getTexture()->getSize().x / 2, pSprite.getTexture()->getSize().y / 2);
+}
+
+Pawn::~Pawn() = default;
+
+void Pawn::canMove() {
+    std::cout << "Doar in fata\n";
+}
+
+
+/// Class Knight(int)side
+Knight::Knight(const Side &side) {
+    this->side = side;
+    code = (1 << 3) + (int) side;
+    pSprite.setTexture(side == Side::WHITE ? Textures::whiteKnight : Textures::blackKnight);
+    pSprite.setOrigin(pSprite.getTexture()->getSize().x / 2, pSprite.getTexture()->getSize().y / 2);
+}
+
+Knight::~Knight() {
+
+}
+
+void Knight::canMove() {
+    Piece::canMove();
+}
+
+
+/// Class Bishop
+Bishop::Bishop(const Side &side) {
+    this->side = side;
+    code = (1 << 4) + (int) side;
+    pSprite.setTexture(side == Side::WHITE ? Textures::whiteBishop : Textures::blackBishop);
+    pSprite.setOrigin(pSprite.getTexture()->getSize().x / 2, pSprite.getTexture()->getSize().y / 2);
+}
+
+Bishop::~Bishop() {
+
+}
+
+void Bishop::canMove() {
+    Piece::canMove();
+}
+
+/// Class Rook
+Rook::Rook(const Side &side) {
+    this->side = side;
+    code = (1 << 5) + (int) side;
+    pSprite.setTexture(side == Side::WHITE ? Textures::whiteRook : Textures::blackRook);
+    pSprite.setOrigin(pSprite.getTexture()->getSize().x / 2, pSprite.getTexture()->getSize().y / 2);
+}
+
+Rook::~Rook() {
+
+}
+
+void Rook::canMove() {
+    Piece::canMove();
+}
+
+/// Class Queen
+Queen::Queen(const Side &side) {
+    this->side = side;
+    code = (1 << 6) + (int) side;
+    pSprite.setTexture(side == Side::WHITE ? Textures::whiteQueen : Textures::blackQueen);
+    pSprite.setOrigin(pSprite.getTexture()->getSize().x / 2, pSprite.getTexture()->getSize().y / 2);
+}
+
+Queen::~Queen() {
+
+}
+
+void Queen::canMove() {
+    Piece::canMove();
+}
+
+/// Class King
+King::King(const Side &side) {
+    this->side = side;
+    code = (1 << 7) + (int) side;
+    pSprite.setTexture(side == Side::WHITE ? Textures::whiteKing : Textures::blackKing);
+    pSprite.setOrigin(pSprite.getTexture()->getSize().x / 2, pSprite.getTexture()->getSize().y / 2);
+}
+
+King::~King() {
+
+}
+
+void King::canMove() {
+    Piece::canMove();
 }
